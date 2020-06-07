9f1cb0a4c8a424088061b50d8a314295bc74b8c5
SidAkalwadi <35581882+SidAkalwadi@users.noreply.github.com>
2020-06-07 15:53:52 -0400

    Add files via upload
    Added colors when tabs are selected or hovered on.

--- animations.js
+++ animations.js
@@ -1,11 +1,16 @@
 function navAnimate(id){
-    var navTabs = document.getElementsByClassName("dropdown-item"); //remove animate from other dropdown items
+    var navTabs = document.getElementsByClassName("nav-tab"); //remove animate from other nav-tabs
     var index;
     for(index = 0; index< navTabs.length; index++){
         navTabs[index].classList.remove("selected");
     }
     
-    var navTabs = document.getElementsByClassName(id);
+    navTabs = document.getElementsByClassName("dropdown-item"); //remove animate from other dropdown items
+    for(index = 0; index< navTabs.length; index++){
+        navTabs[index].classList.remove("selected");
+    }
+    
+    navTabs = document.getElementsByClassName(id);
     for(index = 0; index< navTabs.length; index++){ //add animate to given dropdowns
         navTabs[index].classList.add("selected");
     }
