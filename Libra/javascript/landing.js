00ef75439c6c2f86cba641c9f98121cac81db828
SidAkalwadi <35581882+SidAkalwadi@users.noreply.github.com>
2020-06-11 23:28:11 -0400

    Add files via upload
    New front page graphic is done, complete with the description box. The background has also been photoshopped into parts in order to animate it.

--- Libra/javascript/landing.js
+++ Libra/javascript/landing.js
@@ -1,8 +1,36 @@
 function load(){
-    document.documentElement.style.setProperty("--screen-width", window.innerWidth); //code originally written on a 1920x969 monitor
+    document.documentElement.style.setProperty("--screen-width",window.innerWidth); //code originally written on a 1920x969 monitor
     document.documentElement.style.setProperty("--screen-height", window.innerHeight);
+    slideshow(0);
 }
 
-window.addEventListener("scroll", function(e){
-    document.documentElement.style.setProperty("--anim-del", -window.pageYOffset/((5000/969 * window.innerHeight )-window.innerHeight+20) + 's');
-});
+var prevSlide=5;
+
+function slideshow(slide){
+//    graphic
+    document.getElementById("g" + prevSlide).classList.remove("float");
+    document.getElementById("g" + prevSlide).classList.add("drop");
+    
+//    description
+    document.getElementById("d" + prevSlide).classList.remove("slideIn");
+    
+//    tab
+    document.getElementById("t" + prevSlide).classList.remove("active");
+    
+    prevSlide=slide;
+    
+//    graphic
+    document.getElementById("g" + slide).classList.remove("drop");
+    document.getElementById("g" + slide).classList.add("lift");
+    
+//    description
+    document.getElementById("d" + slide).classList.add("slideIn");
+    
+//    tab
+    document.getElementById("t" + slide).classList.add("active");
+    
+    setTimeout(() => {
+        document.getElementById("g" + slide).classList.remove("lift");
+        document.getElementById("g" + slide).classList.add("float");
+    }, 500);
+}
